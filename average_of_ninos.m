c6059e73c9d0dfed5c2fe5b0ada8ef842b025ca2
lucaswashburn <lucaswashburn13@gmail.com>
2022-01-03 13:39:34 -0800

    yo

--- average_of_ninos.m
+++ average_of_ninos.m
@@ -27,21 +27,24 @@
 % 
 sum_now = 0;
 sum_future = 0;
+firstyear = 2;
+lastyear = 9;
 
 
-for i = 2:10
+for i = firstyear:lastyear
     El_BaseName='El_Nino_12_Years_00';
     El_FileName=[El_BaseName,num2str(i)];
     BaseName='Nino_12_00';
     FileName=[BaseName,num2str(i)];
-    sum_now = sum_now + length(El_FileName)/length(FileName);
+    sum_now = sum_now + length(eval(El_FileName))/length(eval(FileName))/(lastyear-firstyear)*12;
 end
+%sum_now is the frequency of el nino events, unit is 1/year
 
-for i = 2:10
+for i = firstyear:lastyear
     El_BaseName='El_Nino_12_Years_00';
     El_FileName=[El_BaseName,num2str(i),'_1'];
     BaseName='Nino_12_00';
     FileName=[BaseName,num2str(i),'_1'];
-    sum_now_future = sum_now + length(El_FileName)/length(FileName);
+    sum_future = sum_future + length(eval(El_FileName))/length(eval(FileName))/(lastyear-firstyear)*12;
 end
-    
+%sum_future is the frequency of el nino events, unit is 1/year
